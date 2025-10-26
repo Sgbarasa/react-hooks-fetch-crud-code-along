3962d2c0afb38d4fb77d6789c58b632df18fc6b3
stanley <sgbarasa@gmail.com>
2025-10-26 23:42:57 +0300

    final project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React from "react";
 
 function Header({ isDarkMode, onDarkModeClick }) {
+  const buttonText = isDarkMode ? "Light Mode" : "Dark Mode";
   return (
     <header>
       <h2>Shopster</h2>
-      <button onClick={onDarkModeClick}>
-        {isDarkMode ? "Dark" : "Light"} Mode
-      </button>
+      <button onClick={onDarkModeClick}>{buttonText}</button>
     </header>
   );
 }
